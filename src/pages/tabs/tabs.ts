875ce56bd1496008a00cff34b710bbefd3dbf4b0
HiperEmmanuel <hiperemmanuel@gmail.com>
2018-04-18 18:02:05 -0600

    Plantilla de juego
    Se agregó la base para que pueda funcionar el juego, hace falta diseño

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
-
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
 import { RecordPage } from '../record/record';
-import { ConfigPage } from "../config/config";
+import { ConfigPage } from '../config/config';
 
 @Component({
   templateUrl: 'tabs.html'
