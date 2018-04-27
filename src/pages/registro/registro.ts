6befeda0538a4bfbc5ba5709d04349354044eeb6
aarmenta <armenttamario@gmail.com>
2018-04-27 12:15:04 -0600

    Added Sign in method

--- src/pages/registro/registro.ts
+++ src/pages/registro/registro.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+import { AuthService } from '../../services/auth.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HomePage } from '../home/home';
+import { TabsPage } from '../tabs/tabs';
 
 /**
  * Generated class for the RegistroPage page.
@@ -14,8 +18,14 @@ import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angul
   templateUrl: 'registro.html',
 })
 export class RegistroPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams, private view: ViewController) {
+  signupError: string;
+  form: FormGroup;
+  
+  constructor(fb: FormBuilder, public navCtrl: NavController, public navParams: NavParams, private view: ViewController, private auth: AuthService) {
+    this.form = fb.group({
+			email: ['', Validators.compose([Validators.required, Validators.email])],
+			password: ['', Validators.compose([Validators.required, Validators.minLength(6)])]
+		});
   }
 
   ionViewDidLoad() {
@@ -25,5 +35,17 @@ export class RegistroPage {
   cerrarModal(){
     this.view.dismiss();
   }
+
+  signup() {
+		let data = this.form.value;
+		let credentials = {
+			email: data.email,
+			password: data.password
+		};
+		this.auth.signUp(credentials).then(
+			() => this.navCtrl.setRoot(HomePage),
+			error => this.signupError = error.message
+		);
+}
 
 }
