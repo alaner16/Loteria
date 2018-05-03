6f0109528bb57cbb70ec8f18c7ec08bfbc95e7a3
Unknown <zicaxzx@gmail.com>
2018-05-02 22:46:24 -0600

    salen las cartas

--- src/pages/juego/juego.ts
+++ src/pages/juego/juego.ts
@@ -5,7 +5,7 @@ import { PartidaProvider } from '../../providers/partida/partida';
 import { TableProvider } from '../../providers/partida/table';
 import * as firebase from 'firebase';
 import { Observable } from 'rxjs/Observable';
-import { HomePage } from '../home/home';
+import 'rxjs/add/observable/interval';
 
 
 /**
@@ -27,34 +27,45 @@ export class JuegoPage {
   public tables: any;
   public table: any;
   user:any;
+  sub:any;
+  game:any;
+  game_id:any;
+  indice = 0;
+  intervalito = 1;
 
   constructor(public navCtrl: NavController,public partidaService: PartidaProvider, public navParams: NavParams, private modal: ModalController, private tableService: TableProvider) {
+    this.game = {random: [0,0,0]}
   }
 
   ionViewWillLeave(){
     this.user= firebase.auth().currentUser;
     this.partidaService.leaveGame(this.user);
-    let elem = <HTMLElement>document.querySelector(".tabbar");
-    if (elem != null) {
-      elem.style.display = 'flex';
-    }
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad JuegoPage');
-    let elem = <HTMLElement>document.querySelector(".tabbar");
-    if (elem != null) {
-      elem.style.display = 'none';
-    }
     this.play();
+    this.partidaService.getGame(this.game_id).then( aa => {
+      this.game = aa;
+      this.intervalito = Number(this.game.settings.cardtimer);
+    });
+    this.sub = Observable.interval(1000*this.intervalito).subscribe((val) => {
+      this.partidaService.getGame(this.game_id).then( aa => {
+        this.game = aa;
+        console.log(this.game.random[this.indice]);
+        this.indice ++;
+        if(this.indice>15){
+          this.indice = 0;
+        }
+      });
+     });
   }
 
   card(id){console.log( id )}
 
   play(){
     this.tb=this.navParams.get('tabla');
-    console.log(this.tb);
-
+    this.game_id = this.navParams.get('game');
     this.tableService.getTables().then(response =>{
       this.tables = response;
       this.table = this.tables[this.tb]
@@ -66,10 +77,6 @@ export class JuegoPage {
   abrirChat(){
     const modalChat = this.modal.create(ChatPage);
     modalChat.present();
-  }
-
-  salir(){
-    this.navCtrl.setRoot(HomePage);
   }
 
 }
