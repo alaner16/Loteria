d9a6fa0a07c0da5696e4efd3f4e310f3accb8c01
Ernesto Espinoza <ealonso1996@hotmail.com>
2018-05-06 04:49:08 -0600

    Se repararon bugs del chat (scroll, user, hora)

--- src/pages/chat/chat.ts
+++ src/pages/chat/chat.ts
@@ -21,20 +21,23 @@ export class ChatPage {
   messages = [];
   public user: any;
   public email: any;
+  public date: any = new Date();
   constructor(public navCtrl: NavController, public navParams: NavParams, private view: ViewController) {
     this.getMessages();
     this.user= firebase.auth().currentUser;
     this.email = this.user.email;
+    
   }
 
   getMessages(){
     var messagesRef = firebase.database().ref().child("messages");
     messagesRef.on("value", (snap) => {
       var data = snap.val();
+      this.messages = [];
       for(var key in data){
         this.messages.push(data[key]);
       }
-      console.log(this.email);
+
       this.scrollToBottom();
     });
   }
@@ -43,13 +46,15 @@ export class ChatPage {
     var contentEnd = document.getElementById("content-end").offsetTop;
     this.content.scrollTo(0, contentEnd, 300);
   }
-  //ionViewDidLoad() {
-    //console.log('ionViewDidLoad ChatPage');
-  //}
+ ionViewDidLoad() {
+    console.log('ionViewDidLoad ChatPage');
+  }
 
   sendMessage(){
+    this.date = Date.now();
+    console.log();
     var messagesRef = firebase.database().ref().child("messages");
-    messagesRef.push({mensaje: this.message, nombre: this.email });
+    messagesRef.push({mensaje: this.message, nombre: this.email, hora: this.date });
     this.message = "";
   }
   cerrarModal(){
